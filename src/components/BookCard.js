474d40d754f380c40a1643682909cac7b21bb789
Dan <65185320+Dbagrawat@users.noreply.github.com>
2020-07-15 23:38:53 +0100

    Added react-bootstrap elements on the page

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -1,33 +1,35 @@
 import React from "react";
+import { Col, Card } from "react-bootstrap";
 
 const BookCard = (props) => {
     return props.data.books.map((book) => (
-        <div className="col-3 mb-4" key={book.id}>
-            <div className="card w-100 h-100">
-                <div className="card-body">
+        <Col
+            className="col-xs-12 col-sm-6 col-md-4 col-lg-3 mb-4"
+            key={book.id}
+        >
+            <Card className="w-100 h-100">
+                <Card.Body>
                     <img
                         className="card-img-top mb-4"
                         src="https://via.placeholder.com/150"
                         alt="Card image cap"
                     />
-                    <h5 className="card-title">
-                        Book Title: {book.book_title}
-                    </h5>
-                    <p className="card-text">Book pages: {book.book_pages}</p>
-                    <p className="card-text">
+                    <Card.Title>Book Title: {book.book_title}</Card.Title>
+                    <Card.Text>Book pages: {book.book_pages}</Card.Text>
+                    <Card.Text>
                         Publication country: {book.book_publication_country}
-                    </p>
-                    <p className="card-text">
+                    </Card.Text>
+                    <Card.Text>
                         Publication city: {book.book_publication_city}
-                    </p>
-                </div>
-                <div className="card-footer">
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
                     <small className="text-muted">
                         Publication year: {book.book_publication_year}
                     </small>
-                </div>
-            </div>
-        </div>
+                </Card.Footer>
+            </Card>
+        </Col>
     ));
 };
 
